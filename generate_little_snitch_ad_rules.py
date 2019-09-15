05590ed77c118328ebc5ee0d2bdca98c6b74db37
Leonardo Castorina <leonardo.castorina98@gmail.com>
2019-09-15 10:37:07 +0100

    Clean up bad code

--- generate_little_snitch_ad_rules.py
+++ generate_little_snitch_ad_rules.py
@@ -1,4 +1,3 @@
-## Note: this version is for Google Colab notebooks.
 
 ## Read host file from Steven Black (adware + malware)
 
@@ -6,7 +5,7 @@ import urllib.request
 import io
 import re
 
-HOST_FILE_URL = "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
+HOST_FILE_URL = 'https://hosts-file.net/ad_servers.txt'
 
 u = urllib.request.urlopen(HOST_FILE_URL, data = None)
 f = io.TextIOWrapper(u, encoding="utf-8")
@@ -17,7 +16,7 @@ text = f.readlines()
 
 # Only keep hostnames that follow 0.0.0.0, while removing trailing comments.
 # First group is the hostname, second (optional) group is the comment.
-p = re.compile("^0.0.0.0 ([^#]*)(#.*)?")
+p = re.compile("^127.0.0.1 ([^#]*)(#.*)?")
 
 rules = []
 
@@ -25,7 +24,7 @@ for line in text:
   m = p.search(line)
   if m is not None:
     hostname = m.group(1).strip()
-    if hostname != "0.0.0.0":
+    if hostname != "127.0.0.1":
       # Append rule to block the host
       rules.append({"action":"deny", "process":"any", "remote-domains":hostname})
       
@@ -33,9 +32,8 @@ for line in text:
 ## Generate JSON file for little snitch
 
 import json
-from google.colab import files
 
-OUT_FILENAME = "adware_malware_list.lsrules"
+OUT_FILENAME = "hphosts.lsrules"
 
 # Create a standard JSON structure for Little Snitch
 data = {}
@@ -46,5 +44,5 @@ data["rules"] = rules
 
 # Write JSON to file and download
 json_data = json.dumps(data, indent=2)
-open(OUT_FILENAME, "w").write(json_data)
-files.download(OUT_FILENAME)
+with open(OUT_FILENAME, 'w+') as out_file:
+  out_file.write(json_data)
